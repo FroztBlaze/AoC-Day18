12e11f27a7b40bc44ee31d8998acd3d4631e1849
FroztBlaze <omgitscabel@gmail.com>
2023-04-03 22:54:30 +0100

    Added a test for the CSV splitter function

--- Day18/Day18.cpp
+++ Day18/Day18.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <cassert>
+#include "../headers/utils.h"
+
+bool split_csv_test() {
+	std::string testring("1,2,,3,,\n       \t   ");
+	std::vector<std::string> desired_output{ "1","2","","3","" };
+
+	std::vector<std::string> output = Utils::split_line_csv(testring);
+
+	if (desired_output == output) {
+		std::cout << "split_csv_test: PASSED.\n";
+		return true;
+	}
+	else {
+		std::cout << "split_csv_test: FAILED.\n";
+		return false;
+	}
+}
 
 int main()
 {
-    //std::cout << "Result: ";
+	assert(split_csv_test());
+
+	std::cout << "\nAll tests ran successfully.\n";
 }
